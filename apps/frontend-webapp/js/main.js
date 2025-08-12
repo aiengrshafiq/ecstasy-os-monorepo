edc79634893ebcc3759b3b8c8c0971fdfc9b0533
Muhammad Shafiq <engr.laravel@gmail.com>
2025-08-12 16:06:33 +0400

    url changes

--- apps/frontend-webapp/js/main.js
+++ apps/frontend-webapp/js/main.js
@@ -4,7 +4,8 @@
 document.addEventListener('DOMContentLoaded', () => {
 
     // --- 1. CONFIGURATION & STATE ---
-    const API_BASE_URL = 'http://127.0.0.1:8000';
+    //const API_BASE_URL = 'http://127.0.0.1:8000';
+    const API_BASE_URL = 'https://ecstasyos-hrms-api.azurewebsites.net';
 
     const AppState = {
         currentUser: null,
